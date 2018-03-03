e0c0459625e70267ba741dbcbf3e4d21ac36f164
Gfred <gfredtech@gmail.com>
2018-03-03 19:09:17 +0300

    Bug fixes and improvements

--- src/main/java/org/user_interface/ui/Bot.java
+++ src/main/java/org/user_interface/ui/Bot.java
@@ -1,5 +1,6 @@
 package org.user_interface.ui;
 
+import org.storage.SearchParameters;
 import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.user_interface.commands.Command;
@@ -16,6 +17,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
@@ -27,7 +29,11 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         Message message = update.getMessage();
+
+
         if (update.hasMessage() && message.hasText()) {
+
+            Long chatId = message.getChatId();
             String x = message.getText();
             Command command;
             switch (x) {
@@ -38,7 +44,7 @@ public class Bot extends TelegramLongPollingBot {
                     reply.setReplyMarkup(new ReplyKeyboardRemove()); // hides keyboard in case it's showing already
                     try {
                         execute(reply);
-                        previous = "/start";
+                        previous.put(chatId, "/start");
 
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
@@ -49,10 +55,13 @@ public class Bot extends TelegramLongPollingBot {
                 case "/login":
                     command = new LoginCommand(message.getFrom(), message.getChat());
 
+                    SendMessage login = command.run();
+                    login.setReplyMarkup(new ReplyKeyboardRemove()); // hides keyboard in case it's showing already
+
                     try {
-                        command.run().setReplyMarkup(new ReplyKeyboardRemove()); // hides keyboard in case it's showing already
-                        execute(command.run());
-                        previous = "/login";
+
+                        execute(login);
+                        previous.put(chatId, "/login");
 
                     } catch (TelegramApiException ex) {
                         ex.printStackTrace();
@@ -61,71 +70,97 @@ public class Bot extends TelegramLongPollingBot {
 
                 case "/signup":
                     command = new SignUpCommand(message.getFrom(), message.getChat());
+
+                    SendMessage signup = command.run();
+                    signup.setReplyMarkup(new ReplyKeyboardRemove()); // hides keyboard in case it's showing already
                     try {
-                        command.run().setReplyMarkup(new ReplyKeyboardRemove()); // hides keyboard in case it's showing already
-                        execute(command.run());
-                        previous = "/signup";
+
+                        execute(signup);
+                        previous.put(chatId, "/signup");
                     } catch (TelegramApiException ex) {
                         ex.printStackTrace();
                     }
                     break;
 
                 default:
-                    if(previous!= null && previous.equals("/signup")) {
+                    if(previous.get(chatId).equals("/signup")) {
 
                         signUpName = message.getText();
-                        sendMessage(message.getChatId(), "Enter your e-mail address");
-                        previous = "/signup_name";
+                        sendMessage(chatId, "Enter your e-mail address");
+                        previous.put(chatId, "/signup_name");
 
                     } else
-                    if(previous!= null && previous.equals("/signup_name")) {
+                    if(previous.get(chatId).equals("/signup_name")) {
                         signUpEmail = message.getText();
-                        sendMessage(message.getChatId(), "Enter your phone number");
-                        previous = "/signup_email";
+                        sendMessage(chatId, "Enter your phone number");
+                        previous.put(chatId, "/signup_email");
 
 
 
 
-                    } else if(previous!= null && previous.equals("/signup_email")) {
+                    } else if(previous.get(chatId).equals("/signup_email")) {
                         signUpPhone = message.getText();
 
-                        sendMessage(message.getChatId(), "Set a password for your account");
-                        previous = "/signup_done";
+                        sendMessage(chatId, "Set a password for your account");
+                        previous.put(chatId, "/signup_done");
 
-                    } else if(previous!= null && previous.equals("/signup_done")) {
+                    } else if(previous.get(chatId).equals("/signup_done")) {
                         signUpPassword = message.getText();
                         System.out.println(signUpName + " " + signUpEmail + " " + signUpPhone + " " + signUpPassword);
                         String accountDetails = "Name: " + signUpName +
-                                "\nEmail: " + signUpEmail + "\nPhone Number: " + signUpPhone;
-                        setInlineKeyBoard(message.getChatId(), accountDetails);
+                                "\n\nEmail: " + signUpEmail + "\n\nPhone Number: " + signUpPhone;
+                        setInlineKeyBoard(chatId, accountDetails, new ArrayList<String>() {{
+                            add("Confirm");
+                            add("Cancel");
+                        }});
 
 
                     }
 
-                    else if (previous!= null && previous.equals("/login")) {
+                    else if (previous.get(chatId).equals("/login")) {
                         username = message.getText();
-                        sendMessage(message.getChatId(), "Enter password");
-                        previous = "/username";
+                        sendMessage(chatId, "Enter password");
+                        previous.put(chatId, "/username");
 
-                    } else if (previous!= null && previous.equals("/username")) {
+                    } else if (previous.get(chatId).equals("/username")) {
                         password = message.getText();
                         if (username.equals("admin") && password.equals("root")) {
-                            showKeyboard(message.getChatId());
-                            previous = "/menu";
+                            showMainMenuKeyboard(chatId, true);
+                            previous.put(chatId, "/menu");
 
                         } else {
-                            sendMessage(message.getChatId(), "Unsuccessful. Please try again.");
-                            previous = "/start";
+                            sendMessage(chatId, "Unsuccessful. Please try again.");
+                            previous.put(chatId, "/start");
+                            
                         }
 
-                    } else
-                        if(message.getText().equals("Checkout document")) {
-                            if (previous!= null && previous.equals("/menu")) {
-                                sendMessage(message.getChatId(), "Enter the name of the document you want to check out");
+                    } else if(previous.get(chatId).equals("/checkout")) {
+                        String bookName = message.getText();
+                        //TODO: search for book. return list of books that are similar to the book being searched for
+                        previous.put(chatId, "book_list");
+
+
+                    }
+                    else if(message.getText().equals("Checkout document")) {
+                            if (previous.get(chatId).equals("/menu")) {
+                                sendMessage(chatId, "Enter the name of the document you want to check out");
+                                previous.put(chatId, "/checkout");
                             } else {
-                                sendMessage(message.getChatId(), "Please /login or /signup first before you can execute this command.");
+                                sendMessage(chatId, "Please /login or /signup first before you can execute this command.");
+                                previous.put(chatId, "/fail");
                             }
-                        }
+                        } else if(previous.get(chatId).equals("/checkout")) {
+
+                    } else if(message.getText().equals("Edit")) {
+                       showCRUDkeyboard(chatId);
+                       previous.put(chatId, "/edit");
+                    } else if(message.getText().equals("Edit/Modify Document")) {
+                        //TODO: Edit, modify, delete document
+
+                    } else if(message.getText().equals("Edit/Modify User")) {
+
+                    }
+
                 break;
 
             }
@@ -134,13 +169,18 @@ public class Bot extends TelegramLongPollingBot {
             String call_data = update.getCallbackQuery().getData();
             Long ChatId =  update.getCallbackQuery().getMessage().getChatId();
 
-            if(call_data.equals("confirm")) {
+            if(call_data.equals("Confirm")) {
                 createAccount(signUpName, signUpEmail, signUpPhone);
+                previous.put(ChatId, "/Confirm");
                 sendMessage(ChatId,
                         "Account created successfully! Use /login to login to your account");
-            } else if(call_data.equals("cancel")) {
+            } else if(call_data.equals("Cancel")) {
+                if(previous.get(ChatId).equals("/Confirm")) {
+                    sendMessage(ChatId, "Already signed up");
+                } else {
                 sendMessage(ChatId,
                         "Signup cancelled. Use /login, or /signup again if you want to create an account");
+            }
             }
         }
     }
@@ -155,7 +195,7 @@ public class Bot extends TelegramLongPollingBot {
         return "404457992:AAE0dHw07sHw8woSFiMJSebrQCK2aUyN8CM";
     }
 
-    void showKeyboard(Long chatId) {
+    void showMainMenuKeyboard(Long chatId, boolean isLibrarian) {
         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
 
         List<KeyboardRow> keyboard = new ArrayList<>();
@@ -167,6 +207,12 @@ public class Bot extends TelegramLongPollingBot {
         row = new KeyboardRow();
         row.add("\uD83D\uDD0ESearch");
         keyboard.add(row);
+
+        if(isLibrarian) {
+            row = new KeyboardRow();
+            row.add("Edit");
+            keyboard.add(row);
+        }
 
         row = new KeyboardRow();
         row.add("⚙️Settings");
@@ -202,22 +248,22 @@ public class Bot extends TelegramLongPollingBot {
         //TODO: add account to database
     }
 
-    void setInlineKeyBoard(Long ChatId, String message) {
+    void setInlineKeyBoard(Long ChatId, String message, List<String> commands) {
         SendMessage msg = new SendMessage();
-        msg.setText(message);
         msg.setChatId(ChatId);
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rows = new ArrayList<>();
         List<InlineKeyboardButton> row = new ArrayList<>();
-        row.add(new InlineKeyboardButton().
-                setText("Confirm").setCallbackData("confirm"));
-        row.add(new InlineKeyboardButton()
-                .setText("Cancel").setCallbackData("cancel"));
-
-        rows.add(row);
+        for(String i: commands) {
+            row.add(new InlineKeyboardButton().
+                    setText(i).setCallbackData(i.trim()));
+            rows.add(row);
+            row = new ArrayList<>();
+        }
         markup.setKeyboard(rows);
         msg.setReplyMarkup(markup);
-
+        msg.setChatId(ChatId);
+        msg.setText(message);
         try {
             execute(msg);
         }catch (TelegramApiException e) {
@@ -227,7 +273,32 @@ public class Bot extends TelegramLongPollingBot {
 
     }
 
-    String previous;
+    void showCRUDkeyboard(Long ChatId) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+        row.add("Edit/Modify Document");
+        keyboard.add(row);
+
+        row = new KeyboardRow();
+        row.add("Edit/Modify User");
+        keyboard.add(row);
+
+        keyboardMarkup.setKeyboard(keyboard);
+
+        SendMessage message = new SendMessage().setChatId(ChatId).setText("Success!");
+        message.setReplyMarkup(keyboardMarkup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    HashMap<Long, String> previous = new HashMap<>();
 
     String signUpName, signUpEmail, signUpPhone, signUpPassword;
 }
