9accb04004cd46c507ee37d07016374a0ff87174
Godfred Asamoah <gfredtech@gmail.com>
2018-03-24 12:04:47 +0300

    Update EditCommand.java

--- src/main/java/org/user_interface/commands/EditCommand.java
+++ src/main/java/org/user_interface/commands/EditCommand.java
@@ -1,196 +1,141 @@
 package org.user_interface.commands;
 
-import org.resources.Book;
-import org.resources.BookFactory;
-import org.resources.Item;
-import org.resources.User;
+import org.resources.*;
 import org.storage.QueryParameters;
 import org.storage.SqlStorage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.AbsSender;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class EditCommand extends Command {
     @Override
     public String run(AbsSender sender, Update update, String info) {
-        BookFactory bookFactory = new BookFactory();
         Long chatId;
         if (update.hasMessage()) chatId = update.getMessage().getChatId();
         else chatId = update.getCallbackQuery().getMessage().getChatId();
+
         switch (info) {
-            case "edit_start":
+            case "startnext":
                 keyboardUtils.showCRUDkeyboard(sender, update);
-                System.out.println("keyboard shown");
                 return "edit_main";
 
-            case "Edit Document":
-                System.out.println("next code after keyboard shown");
-                String input = null;
-                input = update.getMessage().getText();
-
-                if(input != null && input.equals("Edit Document")) {
-                    sendMessage(sender, update, "Select the type of document you want to edit");
-                    keyboardUtils.setInlineKeyBoard(sender, update, "Types:",
-                            new ArrayList<String>() {{
-                                add("Edit Book");
-                                add("Edit Av Material");
-                                add("Edit Journal Issue");
-                                add("Edit Journal Article");
-                            }});
+            case "main":
+                String message = update.getMessage().getText();
+                if (message != null && message.equals("Edit Document")) {
+                    keyboardUtils.showEditDocumentKeyboard(sender, update);
                     return "edit_documenttype";
+                } else if (message != null && message.equals("Edit User")) {
+                    //TODO: EDIT users
+                    return "edit_users";
                 }
 
-            case "Edit Book":
-                sendMessage(sender, update, "Here's a list of books. Select the one you'd like to edit");
-                listBooks(sender, update);
-                return "edit_booklist";
+            case "documenttype":
+                message = update.getMessage().getText();
+                switch (message) {
+                    case "Book":
+                        sendMessage(sender, update, "Here's a list of books. " +
+                                "Select the one you'd like to edit");
+                        listBooks(sender, update);
+                        return "edit_booklist";
 
-            case "edit_booklist":
-                System.out.println("kwasiabeema");
-                Book book = null;
-                input = update.getMessage().getText();
-                int index = Integer.parseInt(input);
-                book = SqlStorage.getInstance().findBooks(new QueryParameters()).get(index - 1);
+                    case "AV Material":
+                        sendMessage(sender, update, "Here's a list of AV Materials. Select the " +
+                                "one you'd like to edit.");
+                        listAvMaterials(sender, update);
+                        return "edit_avmateriallist";
 
-                if (book != null) {
-                    bookCursor.put(chatId, book);
-                    String accountDetails = "Name: " + book.getTitle() +
-                            "\nNo. of copies: " + book.getCopiesNum() + "\nAuthors:" + book.getAuthors() +
-                            "\nPublicationDate: " + book.getPublicationDate();
+                    case "Journal Issue":
+                        sendMessage(sender, update,
+                                "Here's a list of Journal Issues. Select the " +
+                                        "one you'd like to edit");
+                        listJournalIssues(sender, update);
+                        return "edit_journalissuelist";
 
-                    sendMessage(sender, update, accountDetails);
-                    String parameterlist = "title `Name` \ncopies `new number of copies` \n" +
-                            "*If you want to delete the book, type delete*";
-                    sendMessage(sender, update, "This is the parameter list for editing a use book; first type the key of the " +
-                            "parameter you want to edit, followed by its new value, then separate each with a comma(,)");
-                    sendMessage(sender, update, parameterlist);
-                    return "edit_bookparams";
-                }
-                break;
-
-            case "edit_bookparams":
-                Book currentBook = bookCursor.get(chatId);
-                editNumberOfCopies = currentBook.getCopiesNum();
-                editTitle = currentBook.getTitle();
-                System.out.println("*** " + currentBook.getTitle());
-
-                input = update.getMessage().getText();
-                if (input.equals("delete")) {
-                    SqlStorage.getInstance().removeBook(currentBook.getId());
-                    keyboardUtils.showMainMenuKeyboard(sender, update, currentUser.get(chatId),
-                            currentBook.getTitle() + " removed successfully");
-                    return "menu";
+                    case "Journal Article":
+                        sendMessage(sender, update,
+                                "Here's a list of Journal Articles. Select the " +
+                                        "one you'd like to edit");
+                        listJournalArticles(sender, update);
+                        return "edit_journalarticlelist";
                 }
 
-                List<String> items = Arrays.asList(input.split("\\s*,\\s*"));
-                String temp;
-                for(String i: items) {
-                    if(i.startsWith("title")) {
-                        temp = i.replace("title ", "");
-                        SqlStorage.getInstance().updateBook(
-                                currentBook.getId(),
-                                new QueryParameters().add("title", temp));
+            case "booklist":
+                selectDocToEdit(sender, update, chatId, "book");
+                return "edit_params";
 
-                    } else if(i.startsWith("copies")) {
-                        temp = i.replace("copies ", "");
-                        SqlStorage.getInstance().updateBook(
-                                currentBook.getId(),
-                                new QueryParameters().add("copy_num", Integer.parseInt(temp)));
-                    }
-                }
-                keyboardUtils.showMainMenuKeyboard(sender, update, currentUser.get(chatId),
-                        "Edit done successfully");
+            case "avmateriallist":
+                selectDocToEdit(sender, update, chatId, "avmaterial");
+                return "edit_params";
 
-                break;
+            case "journalissuelist":
+                selectDocToEdit(sender, update, chatId, "journalissue");
+                return "edit_params";
 
+            case "journalarticlelist":
+                selectDocToEdit(sender, update, chatId, "journalarticlelist");
+                return "edit_params";
 
-            case "edit_userlist":
-                String msg = update.getMessage().getText();
+            case "params":
+                parseParameters(sender, update, chatId);
+                return "menu_main";
 
-                int position;
-                try {
-                    position = Integer.parseInt(msg);
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                    sendMessage(sender, update, "Input is not a number.");
-                    break;
-                }
-
-                userCursor = SqlStorage.getInstance().findUsers(new QueryParameters()).get(position - 1);
-
-                if (userCursor != null) {
-                    String accountDetails = "Name: " + userCursor.getName() +
-                            "\nAddress: " + userCursor.getAddress() + "\nPhone Number: " + userCursor.getPhoneNumber()
-                            + "\nType:" + userCursor.getSubtype() + "\nCard number: " + userCursor.getCardNumber();
-                    sendMessage(sender, update, accountDetails);
-                    String parameterlist = "name `Name`\n address `Address`\n phone `Phone Number`\n type `Type`\n\n" +
-                            "*If you want to delete a user, type `delete` followed by user's card number";
-                    sendMessage(sender, update, "This is the parameter list for editing a user; first type the key of the " +
-                            "parameter you want to edit, followed by its new value, then separate each with a comma(,)");
-                    sendMessage(sender, update, parameterlist);
-                    return "edit_userparams";
-                }
-
-                break;
-
-            case "edit_userparams":
-                editNumberOfCopies = bookCursor.get(chatId).getCopiesNum();
-                input = update.getMessage().getText();
-                if (input.startsWith("delete")) {
-                    input.replace("delete", "");
-                    SqlStorage.getInstance().removeUser(userCursor.getCardNumber());
-                    keyboardUtils.showMainMenuKeyboard(sender, update, currentUser.get(chatId),
-                            userCursor.getName() + " removed successfully");
-                    return "menu";
-                }
-                items = Arrays.asList(input.split("\\s*,\\s*"));
-                for(String i: items) {
-                    if(i.startsWith("name")) {
-                        temp = i.substring(i.lastIndexOf("name"), i.length());
-                        QueryParameters params = new QueryParameters().add("name", temp);
-                        SqlStorage.getInstance().updateUser(userCursor.getCardNumber(), params);
-
-                    } else if(i.startsWith("address")) {
-                        temp = i.substring(i.lastIndexOf("address"), i.length());
-                        QueryParameters params = new QueryParameters().add("address", temp);
-                        SqlStorage.getInstance().updateUser(userCursor.getCardNumber(), params);
-
-
-                    } else if(i.startsWith("type")) {
-                        temp = i.substring(i.lastIndexOf("type"), i.length());
-                        QueryParameters params = new QueryParameters();
-                        if(temp.equals("Student") || temp.equals("Faculty")) {
-                            params.add("type", "Patron").add("subtype", temp);
-                        } else if(temp.equals("Librarian")) {
-                            params.add("type", temp);
-                        }
-                        SqlStorage.getInstance().updateUser(userCursor.getCardNumber(), params);
-                    }
-                }
-
-                keyboardUtils.showMainMenuKeyboard(sender, update, currentUser.get(chatId),
-                        "Edit done successfully");
         }
         return null;
     }
 
-    public void getCurrentUser(Update update, User user) {
-        Long chatId = update.getMessage().getChatId();
-        currentUser.put(chatId, user);
+    private void parseParameters(AbsSender sender, Update update, Long chatId) {
+        String input = update.getMessage().getText().trim();
+
+        switch (documentCursor.get(chatId).getType()) {
+            case "book":
+                editParser.parseBookParameters(sender, update, chatId, input);
+                break;
+            case "avmaterial":
+                editParser.parseAvMaterialParameters(input);
+                break;
+            case "journalissue":
+                editParser.parseJournalIssueParameters(input);
+                break;
+            case "journalarticle":
+                editParser.parseJournalArticleParameters(input);
+                break;
+        }
+
     }
 
-    HashMap<Long, User> currentUser = new HashMap<>();
-    User userCursor;
+    private void selectDocToEdit(AbsSender sender, Update update, Long chatId, String type) {
+        Item item = null;
+        String input = update.getMessage().getText();
+        int index = Integer.parseInt(input);
+        switch (type) {
+            case "book":
+                item = SqlStorage.getInstance().findBooks(new QueryParameters()).get(index - 1);
+                break;
+            case "avmaterial":
+                item = SqlStorage.getInstance().findAvMaterials(new QueryParameters()).get(index - 1);
+                break;
+            case "journalissue":
+                item = SqlStorage.getInstance().findJournals(new QueryParameters()).get(index - 1);
+                break;
+            case "journalarticle":
+                item = SqlStorage.getInstance().findArticles(new QueryParameters()).get(index - 1);
+                break;
+        }
 
-    HashMap<Long, Book> bookCursor = new HashMap<>();
+        if (item != null) {
+            documentCursor.put(chatId, item);
+            sendMessage(sender, update, item.toString());
 
-    String editTitle;
-    int editNumberOfCopies;
-    LocalDate publicationDate;
+            String parameterlist = "title `Name` \ncopies `new number of copies` \n" +
+                    "*If you want to delete the item, type delete*";
+            sendMessage(sender, update, "This is the parameter list for editing a use book; first type the key of the " +
+                    "parameter you want to edit, followed by its new value, then separate each with a comma(,)");
+            sendMessage(sender, update, parameterlist);
+
+        }
+    }
+
+    static HashMap<String, String> editParams = new HashMap<>();
+    static EditParser editParser = new EditParser();
 
 }
