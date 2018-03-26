bad6f50e0687dcaf28bf5126f09e62ad4c41539e
Gfred <gfredtech@gmail.com>
2018-03-26 10:19:58 +0300

    Added missing file Main.java

---
+++ src/main/java/org/user_interface/Main.java
@@ -0,0 +1,23 @@
+package org.user_interface;
+
+import org.storage.SqlStorage;
+import org.user_interface.ui.Bot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            SqlStorage.connect("postgres", "postgres", "minerva");
+            botsApi.registerBot(new Bot());
+
+        } catch (TelegramApiException|ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
