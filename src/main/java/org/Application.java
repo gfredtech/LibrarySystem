98216a4ce3641f546260711617c065684496cfba
Harrm <harrm@mail.ru>
2018-03-27 17:28:12 +0300

    Now Application is the main executable class

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,8 +1,28 @@
 package org;
 
 
+import org.storage.SqlStorage;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.user_interface.ui.Bot;
+
 public class Application {
 
     public static void main(String[] args) {
+        String databaseName = args[0];
+        String userName = args[1];
+        String password = args[2];
+
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            SqlStorage.connect(databaseName, userName, password);
+            botsApi.registerBot(new Bot());
+
+        } catch (TelegramApiException |ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
